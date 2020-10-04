201dd593764da46e8604684613016ed653545fec
AroshanaD <aroshana.dtw@gmail.com>
2020-10-05 03:22:17 +0530

    modified main-page and style.css

--- front-end/main_page.php
+++ front-end/main_page.php
@@ -11,63 +11,78 @@
         <link rel = "stylesheet" href = "style.css">
     </head>
     <body>
-        <ul>
-            <li class = "col-1"><a class="active" href="main_page.php"> <img src="icons/home.png"></a></li>
-            <li class = "col-2"><a href="#">About Us</a></li>
-            <li class = "col-2"><a href="#">Contact Us</a></li>
-            <li class = "col-2"><a href="#">Appointments</a></li>
-            <li class = "col-2"><a href="#">Doctor Schedule</a></li>
-        </ul>
-
+        <nav>
+            <ul>
+                <li class = "col-1"><a class="active" href="main_page.php"> <img src="icons/home.png"></a></li>
+                <li class = "col-2"><a href="#">About Us</a></li>
+                <li class = "col-2"><a href="#">Contact Us</a></li>
+                <li class = "col-2"><a href="#">Appointments</a></li>
+                <li class = "col-2"><a href="#">Doctor Schedule</a></li>
+            </ul>
+        </nav>
         <div class = "row">
             <div class = "col-4">
                 <div class = "block-0">
-                    <img src="icons/patient.png">
+                    <a href="#"><img src="icons/patient.png"></a>
                     <a href="#">Patient</a>
                 </div>
             </div>
             <div class = "col-4">
                 <div class = "block-0">
-                    <img src="icons/doctor.png">
+                    <a href="#"><img src="icons/doctor.png"></a>
                     <a href="#">Doctor</a>
                 </div>
             </div>
             <div class = "col-4">
                 <div class = "block-0">
-                    <img src="icons/receptionist.png">
+                    <a href="#"><img src="icons/receptionist.png"></a>
                     <a href="#">Receptionist</a>
                 </div>
             </div>
             <div class = "col-4">
                 <div class = "block-0">
-                    <img src="icons/lab.png">
+                    <a href="#"><img src="icons/lab.png"></a>
                     <a href="#">Lab Technician</a>
                 </div>
             </div>
             <div class = "col-4">
                 <div class = "block-0">
-                    <img src="icons/pharmacy.png">
+                    <a href="#"><img src="icons/pharmacy.png"></a>
                     <a href="#">Pharmacist</a>
                 </div>
             </div>
             <div class = "col-4">
                 <div class = "block-0">
-                    <img src="icons/admin.png">
+                    <a href="#"><img src="icons/admin.png"></a>
                     <a href="#">Supervisor</a>
                 </div>
             </div>
         </div>
+        <footer>
         <div class = "row">
-            <div class = "col-6">
+            <div class = "col-8">
                 <div class = "block-1">
-                    About Us
+                    <h>About Us</h>
+                    <div class="block-1">
+                    <p>Our Hospitals, Sri Lanka</p>
+                    <p>Our Hospitals has embarked on a digital journey to be Sri Lanka’s No.1 Smart Hospital.</p>
+                    <p>We have already introduced many innovative digital health solutions including Tele medicine, Online pharmacy,
+                    Online laboratory portal and Tele physiotherapy.</p>
+                </div>
                 </div>
             </div>
-            <div class = "col-6">
+            <div class = "col-4">
                 <div class = "block-1">
-                    Contact Us
+                    <h>Contact Us</h>
+                    <div class = "block-1">
+                        <p> No.1/A, Colombo Rd,</p>
+                        <p> Galle,</p>
+                        <p> Sri Lanka</p>
+                        <p> 091 1234567 </p>
+                    </div>
                 </div>
             </div>
         </div>
+        </footer>
     </body>
 </html>
