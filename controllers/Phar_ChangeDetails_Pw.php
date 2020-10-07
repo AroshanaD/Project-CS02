d9d54da14b93867040843b8e584d7ff80c97e59e
DanuAnjana <56577570+DanuAnjana@users.noreply.github.com>
2020-10-07 14:51:45 +0530

    error_fixed

--- controllers/Phar_ChangeDetails_Pw.php
+++ controllers/Phar_ChangeDetails_Pw.php
@@ -1,14 +1,12 @@
 <?php
-    class Phar_ChangeDetails_Pw extends CI_controller{
+    class Phar_ChangeDetails_Pw extends Controller{
         public function __construct(){
             parent::__construct();
 
             $this->load->database();
             $this->load->helper('url');
-           // $this->load->model('Reg_Model');
             $this->load->helper(array('form'));
             $this->load->library(array('form_validation'));
-            $this->load->library('pdf');
         }
         public function changeDetails(){
             $this->load->view('change_details_password');
