70d29bd7c06830e75fbf48e5b1d2922cf05b53ec
GayashaDS <56672701+GayashaDS@users.noreply.github.com>
2020-10-10 19:28:29 +0530

    doctor

---
+++ views/doctorprofile.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Doctor</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    
+
+        <nav>
+            <ul>
+                <li class = "col-1"><a class="active" href="#"><img src="img/home.png"></a></li>
+                <li class = "col-2"><a href="#">View Appointments</a></li>
+                <li class = "col-2"><a href="#">View Statistics</a></li>
+            </ul>
+        </nav>
+<hr>
+        <div class = "col-12">
+            <div class = "block-1">
+                <h1>Doctor Profile </h1>
+            </div> <!--block-1-->
+        </div> <!--col-12-->
+
+        <div class = "col-12">
+            <div class = "block-1">
+            <button name="change-details" id = "change-details">Change Details</button>
+            </div> <!--col-12-->
+        </div> <!--block-1-->
+
+    
+    
+</body>
+
+</html>
