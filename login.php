4fc1a5d8de2988564d71fe4cb6b863719546524d
AroshanaD <52098386+AroshanaD@users.noreply.github.com>
2020-10-12 16:24:38 +0530

    added login css

---
+++ login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="form-box">
+                <div class="form-name">Login</div>
+                    <form class="login">
+                        <div class="form-login">
+                            <div class="label">
+                                <label for="id">User ID</label>
+                            </div>
+                            <div class="input">
+                                <input type="text" name="id" required>
+                            </div>
+                            <div class="label">
+                                <label for="password">Password</label>
+                            </div>
+                            <div class="input">
+                                <input type="password" name="passowrd" required>
+                            </div>
+                        </div>
+                            <div class="btn-area"><input type="submit" value="Login" class="submit-btn"></div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
--- views/header.php
+++ views/header.php
@@ -4,5 +4,5 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>HomePage</title>
-        <link rel = "stylesheet" href =<?php echo(Router::base_url().'/files/style.css')?> >
+        <link rel = "stylesheet" href =<?php echo(Router::base_url().'/style.css')?> >
     </head>
