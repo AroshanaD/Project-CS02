c7ce6c24b261e6fb413e9f2a23da5831c5f12800
GayashaDS <56672701+GayashaDS@users.noreply.github.com>
2020-10-15 11:11:09 +0530

    search bar added

---
+++ views/view_test.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>View</title>
+    <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="ss.css">
+    </head>
+
+    <body>
+     
+        <div class ="container-small">
+            View Lab Tests
+        </div>  <!--container-small-->
+        
+        <div class ="container">
+        <div class = "container-2">
+        <table class ="reg-table">
+                <div class="site-search"> 
+                    <form action="#"> 
+                        <input type="text" placeholder=" Your Test Id" name="id"> 
+                        <input type="date" placeholder=" Date" name="date"> 
+                        <button type = "submit">GO</button>
+                    </form> 
+                </div>      <!--site-search-->
+                
+                    <tr>
+                        <th>Test ID</th>
+                        <th>Test Date</th>
+                        <th>Test Category</th>
+                        <th>Update</th>
+                        <th>Delete</th>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </table>
+            </div> <!--form-container-->
+        </div><!--container-->
+        
+
+    </body>
+</html>
