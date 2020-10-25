665b94d3a0ff2d0253b928db16946e2fa72815f6
DanuAnjana <56577570+DanuAnjana@users.noreply.github.com>
2020-10-25 09:58:23 +0530

    added new views

--- views/select_date.php
+++ views/select_date.php
@@ -3,10 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="main.css">
     </head>
-    <body>
-        <div class="container-small">Hospital Management System</div>
+    <body style="background-image: linear-gradient(to left,  #fec007de, rgba(255, 255, 255, 0)), url(icons/undraw_calendar_dutt.png);">
         <div class="doctor-card-large">
             <div class="card-details">
                 <div>
