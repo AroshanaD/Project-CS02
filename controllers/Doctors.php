0703dba980a51658096f2b8a4e0f2c91351f6fa3
GayashaDS <56672701+GayashaDS@users.noreply.github.com>
2020-11-10 07:49:19 +0530

    added model and contoller classes

--- controllers/Doctors.php
+++ controllers/Doctors.php
@@ -8,18 +8,103 @@
 
         public function add(){
             $this->load('views','add_doctor');
+            $model= $this->load('models','doctor_manage');
+
+            if(isset($_POST['Add'])){
+                $password = pwd_generator::pwd_gen();
+
+                $user_details = array('category'=>$_POST['category'], 'id'=>$_POST['id'],
+                                'f_name'=>$_POST['f_name'], 'l_name'=>$_POST['l_name'],
+                                'qualifi' => $_POST['qualifi'],'address'=>$_POST['address'], 
+                                'contact'=>$_POST['contact'],'email'=>$_POST['email'],
+                                'specialization' => $_POST['specialization'],
+                                'fee'=> $_POST['fee'],'password'=>$password);
+
+                $status = $model->add($id,$f_name,$l_name, $qualifi,$address,$contact,$email,$specialization,$fee);
+                
+                if($result == TRUE){
+                    header("Location: ../doctor/view?successfully updated");
+                }
+                else{
+                    header("Location: ../doctor/view?something went wrong");
+                }
+
+            }
         }
 
         public function view(){
+            $this->load('models;','doctor_manage');
+            $result = $model->view();
+            $_POST['doctor']= $result;
             $this->load('views','view_doctor');
+
         }
 
         public function update(){
+            $this->load('models','doctor_manage');
+            if(isset($_GET['category']) && $_GET['id']){
+                $category = $_GET['category'];
+                $id = $_GET['id'];
+                $result = $model->update_current($category,$id);
+                $_POST['details'] = $result;
+            }
             $this->load('views','update_doctor');
+
+            if(isset($_POST['Update'])){
+                $address = $_POST['address'];
+                $contact = $_POST['contact'];
+                $email = $_POST['email'];
+                $fee => $_POST['fee'];
+                $result = $model->update_change($id,$category,$address,$contact,$email);
+
+                if($result == TRUE){
+                    header("Location: ../doctor/view?successfully updated");
+                }
+                else{
+                    header("Location: ../doctor/view?something went wrong");
+                }
+            }
         }
 
         public function delete(){
+            $model = $this->load('models','doctor_manage');
+            if(isset($_GET['category']) && $_GET['id']){
+                $category = $_GET['category'];
+                $id = $_GET['id'];
+                $result = $model->update_current($category,$id);
+                $_POST['details'] = $result;
+            }
             $this->load('views','delete_doctor');
-        }
 
+            if(isset($_POST['Delete'])){
+                $result = $model->delete($category,$id);
+
+                if($result == TRUE){
+                    header("Location: ../doctor/view?successfully deleted");
+                }
+                else{
+                    header("Location: ../doctor/view?something went wrong");
+                }
+            }
+        }
+    }
+
+    public function category(){
+        $model = $this->load('models','doctor_manage');
+
+        $category = $_POST['doctor'];
+        $result = $model->view($category);
+        header('Content-Type: application/json');
+        echo json_encode($result);
+    }
+
+    public function search(){
+        $model = $this->load('models','doctor_manage');
+
+        $category = $_POST['doctor'];
+        $id = $_POST['id'];
+        $name = $_POST['name'];
+        $result = $model->search($category,$id, $name);
+        header('Content-Type: application/json');
+        echo json_encode($result);
     }
---
+++ models/doctor_manage.php
@@ -0,0 +1,63 @@
+<?php
+class doctor_manage extends Models{
+    public function __construct(){
+
+    }
+
+    public function view($category){
+        $connect = new Database();
+        $pdo = $connect->connect();
+
+        $query = "SELECT id,f_name,l_name,qualification,address,contact,email,fee,specialization_id FROM doctor";
+        $stmt = $pdo->prepare($query);
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+    }
+
+    public function search($category,$id,$name){
+        $connect = new Database;
+        $pdo = $connect->connect();
+
+        $query = "SELECT * FROM doctor WHERE id = ? OR name = ?";
+        $stmt = $pdo->prepare($query);
+        $stmt->execute([$id,$name]);
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+    }
+
+    public function add($id,$f_name,$l_name, $qualifi,$address,$contact,$email,$specialization,$fee){
+        $connect = new Database();
+        $pdo = $connect->connect();
+        $query="insert into doctor values('$id','$f_name','$l_name','$qualifi','$address','$contact','$email','$specialization','$fee')";
+        $stmt = $pdo->prepare($query);
+        $stmt->execute();
+    }
+
+    public function displayById($id){
+        $connect = new Database();
+        $pdo = $connect->connect();
+        $query = "select * from doctor where id='".$id."'";
+        $stmt = $pdo->prepare($query);
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+    }
+
+    public function update($id,$f_name,$l_name, $qualifi,$address,$contact,$email,$specialization,$fee){
+        $connect = new Database();
+        $pdo = $connect->connect();
+        $query= "update doctor SET id='$id', f_name='$f_name',l_name='$l_name', qualification='$qualifi',address='$address',contact='$contact,email='$email',specialization='$specialization',fee='$fee' where id='".$id."'";
+        $stmt = $pdo->prepare($query);
+        $stmt->execute();
+    }
+
+    public function delete($id){
+        $connect = new Database();
+        $pdo = $connect->connect();
+        $query = "delete from doctor where id='".$id."'";
+        $stmt = $pdo->prepare($query);
+        $stmt->execute();
+    }
+}
+?>
