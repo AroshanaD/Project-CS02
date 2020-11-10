c2b90ceed4bda6621ce567a30d489ec13f3d4ff6
AroshanaD <52098386+AroshanaD@users.noreply.github.com>
2020-11-11 00:15:56 +0530

    removed redundancy of code in javascript files by creating functions

--- files/js/inventory.js
+++ files/js/inventory.js
@@ -4,21 +4,8 @@ $(document).ready(function(){
         data: {},
         type: 'post',
         success:function(data){
-
-            $("tbody").empty();
-
-            for(var i=0; i<data.length; i++){
-                var get_details = data[i]['id'];
-                var update = "<a href=../inventory/update?id=".concat(get_details,"><button class='tb-btn'>Update</a>");
-                var dele = "<a href=../inventory/delete?id=".concat(get_details,"><button class='tb-btn'>Delete</a>");
-                var row = $(`<tr id=${data[i].id}>`).append($(`<td>`).text(i+1),
-                $(`<td>`).text(data[i].id),$(`<td>`).text(data[i].name),
-                $(`<td>`).text(data[i].vendor),$(`<td>`).text(data[i].description),
-                $(`<td>`).text(data[i].unit_price),$(`<td>`).text(data[i].quantity),
-                $(`<td>`).append(update),
-                $(`<td>`).append(dele));
-                $("tbody").append(row);
-            }
+            var details = data;
+            render_table(details);
         }
     })
 
@@ -28,22 +15,25 @@ $(document).ready(function(){
             data: {id:$("#id").val(),name:$("#name").val()},
             type: 'post',
             success:function(data){
-
-                $("tbody").empty();
-
-                for(var i=0; i<data.length; i++){
-                    var get_details = data[i]['id'];
-                    var update = "<a href=../inventory/update?id=".concat(get_details,"><button class='tb-btn'>Update</a>");
-                    var dele = "<a href=../inventory/delete?id=".concat(get_details,"><button class='tb-btn'>Delete</a>");
-                    var row = $(`<tr id=${data[i].id}>`).append($(`<td>`).text(i+1),
-                    $(`<td>`).text(data[i].id),$(`<td>`).text(data[i].name),
-                    $(`<td>`).text(data[i].vendor),$(`<td>`).text(data[i].description),
-                    $(`<td>`).text(data[i].unit_price),$(`<td>`).text(data[i].quantity),
-                    $(`<td>`).append(update),
-                    $(`<td>`).append(dele));
-                    $("tbody").append(row);
-                }
+                var details = data;
+                render_table(details);
             }
         })
     })
 })
+
+function render_table(data){
+    $("tbody").empty();
+    for(var i=0; i<data.length; i++){
+        var get_details = data[i]['id'];
+        var update = "<a href=../inventory/update?id=".concat(get_details,"><button class='tb-btn'>Update</a>");
+        var dele = "<a href=../inventory/delete?id=".concat(get_details,"><button class='tb-btn'>Delete</a>");
+        var row = $(`<tr id=${data[i].id}>`).append($(`<td>`).text(i+1),
+        $(`<td>`).text(data[i].id),$(`<td>`).text(data[i].name),
+        $(`<td>`).text(data[i].vendor),$(`<td>`).text(data[i].description),
+        $(`<td>`).text(data[i].unit_price),$(`<td>`).text(data[i].quantity),
+        $(`<td>`).append(update),
+        $(`<td>`).append(dele));
+        $("tbody").append(row);
+    }
+}
--- files/js/staff.js
+++ files/js/staff.js
@@ -5,24 +5,8 @@ $(document).ready(function(){
             data: {staff:$("#staff").val()},
             type: 'post',
             success:function(data){
-
-                $("tbody").empty();
-                var category = $("#staff").val();
-
-                for(var i=0; i<data.length; i++){
-                    var get_details = data[i]['id'].concat("&category=",category);
-                    var name = data[i]['f_name'].concat(' ',data[i]['l_name']);
-                    var update = "<a href=../staff/update?id=".concat(get_details,"><button class='tb-btn'>Update</a>");
-                    var dele = "<a href=../staff/delete?id=".concat(get_details,"><button class='tb-btn'>Delete</a>");
-
-                    var row = $(`<tr id=${data[i].id}>`).append($(`<td>`).text(i+1),
-                    $(`<td>`).text(data[i].id),$(`<td>`).text(data[i].f_name.concat(" ",data[i].l_name)),
-                    $(`<td>`).text(data[i].address),$(`<td>`).text(data[i].contact_no),
-                    $(`<td>`).text(data[i].email),
-                    $(`<td>`).append(update),
-                    $(`<td>`).append(dele));
-                    $("tbody").append(row);
-                }
+                var details = data;
+                render_table(details);
             }
         })
     })
@@ -33,26 +17,29 @@ $(document).ready(function(){
             data: {staff:$("#staff").val(),id:$("#id").val(),name:$("#name").val()},
             type: 'post',
             success:function(data){
-
-                $("tbody").empty();
-                $("#id").text("");
-                var category = $("#staff").val();
-
-                for(var i=0; i<data.length; i++){
-                    var get_details = data[i]['id'].concat("&category=",category);
-                    var name = data[i]['f_name'].concat(' ',data[i]['l_name']);
-                    var update = "<a href=../staff/update?id=".concat(get_details,"><button class='tb-btn'>Update</a>");
-                    var dele = "<a href=../staff/delete?id=".concat(get_details,"><button class='tb-btn'>Delete</a>");
-
-                    var row = $(`<tr id=${data[i].id}>`).append($(`<td>`).text(i+1),
-                    $(`<td>`).text(data[i].id),$(`<td>`).text(data[i].f_name.concat(" ",data[i].l_name)),
-                    $(`<td>`).text(data[i].address),$(`<td>`).text(data[i].contact_no),
-                    $(`<td>`).text(data[i].email),
-                    $(`<td>`).append(update),
-                    $(`<td>`).append(dele));
-                    $("tbody").append(row);
-                }
+                var details = data;
+                render_table(details);
             }
         })
     })
 })
+
+function render_table(data){
+    $("tbody").empty();
+    var category = $("#staff").val();
+
+    for(var i=0; i<data.length; i++){
+        var get_details = data[i]['id'].concat("&category=",category);
+        var name = data[i]['f_name'].concat(' ',data[i]['l_name']);
+        var update = "<a href=../staff/update?id=".concat(get_details,"><button class='tb-btn'>Update</a>");
+        var dele = "<a href=../staff/delete?id=".concat(get_details,"><button class='tb-btn'>Delete</a>");
+
+        var row = $(`<tr id=${data[i].id}>`).append($(`<td>`).text(i+1),
+        $(`<td>`).text(data[i].id),$(`<td>`).text(data[i].f_name.concat(" ",data[i].l_name)),
+        $(`<td>`).text(data[i].address),$(`<td>`).text(data[i].contact_no),
+        $(`<td>`).text(data[i].email),
+        $(`<td>`).append(update),
+        $(`<td>`).append(dele));
+        $("tbody").append(row);
+    }
+}
