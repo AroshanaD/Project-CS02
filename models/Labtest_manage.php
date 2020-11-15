5796b8248ca46dff61ce8dbd20158e28295d205e
DanuAnjana <56577570+DanuAnjana@users.noreply.github.com>
2020-11-15 14:13:43 +0530

    update labtest model

--- models/Labtest_manage.php
+++ models/Labtest_manage.php
@@ -10,14 +10,64 @@
             $query="insert into test values('$id','$name','$price','$description',0)";
             $stmt = $pdo->prepare($query);
             $status=$stmt->execute();
+                
+        }
 
-            if($status == TRUE){
-                return TRUE;
+        public function view(){
+            $connect = new Database();
+            $pdo = $connect->connect();
+
+            $query = "SELECT * FROM test WHERE deleted='0'";
+            $stmt = $pdo->prepare($query);
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $result;
+        }
+
+        public function search($id,$name){
+            $connect = new Database;
+            $pdo = $connect->connect();
+    
+            if($name != null){
+                $name = $name.'%';
             }
             else{
-                return FALSE;
+                $name = '';
             }
-                
+    
+            $query = "SELECT * FROM test WHERE id = ? AND deleted ='0' OR name LIKE ?  ";
+            $stmt = $pdo->prepare($query);
+            $stmt->execute([$id,$name]);
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $result;
+        }
+
+        public function displayById($id){
+            $connect = new Database();
+            $pdo = $connect->connect();
+            $query = "SELECT * FROM test WHERE id=?";
+            $stmt = $pdo->prepare($query);
+            $stmt->execute([$id]);
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $result;
+        }
+
+        public function update($id,$testName,$price,$description){
+            $connect = new Database();
+            $pdo = $connect->connect();
+            $deleted = 0;
+            $query= "UPDATE test SET name=?, unit_cost=?, description=?, deleted='$deleted' WHERE id=?";
+            $stmt = $pdo->prepare($query);
+            $status = $stmt->execute([$testName,$price, $description,$id]);
+        }
+
+        public function delete($testId){
+            $connect = new Database();
+            $pdo = $connect->connect();
+
+            $query = "UPDATE test SET deleted='1' WHERE id=?";
+            $stmt = $pdo->prepare($query);
+            $status = $stmt->execute([$testId]);
         }
     }
 ?>
