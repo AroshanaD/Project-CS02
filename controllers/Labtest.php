595366806d816bb762658528f8f947e96cd8cbba
DanuAnjana <56577570+DanuAnjana@users.noreply.github.com>
2020-11-15 14:12:56 +0530

    update labtest controller

--- controllers/Labtest.php
+++ controllers/Labtest.php
@@ -17,20 +17,12 @@
             $model=$this->load('models','Labtest_manage');
             
             if(isset($_POST['Add'])){
-                print("hello danu ");
                 $id=$_POST['test_id'];
                 $name=$_POST['test_name'];
                 $description=$_POST['test_description'];
                 $price=$_POST['test_price'];
 
-                $result= $model->add($id,$name,$price,$description);
-                if($result==TRUE){
-                    header("Location:labtest/view?successfully updated");
-                    $this->view();
-                }
-                else{
-                    header("Location: ../labtest/view?something went wrong");
-                }
+                $model->add($id,$name,$price,$description);
             }
         }
 
@@ -39,14 +31,52 @@
             $this->load('views','view_test');
         }
 
+        public function get_view(){
+            $model= $this->load('models','Labtest_manage');
+            $result = $model->view();
+            
+            header('Content-Type: application/json');
+            echo json_encode($result);
+        }
+
+        public function search(){
+            $model = $this->load('models','labtest_manage');
+            $id = $_POST['id'];
+            $name = $_POST['name'];
+            $result = $model->search($id,$name);
+            header('Content-Type: application/json');
+            echo json_encode($result);
+        }
+
         public function update(){
+            $id = $_GET['id'];
+            $model= $this->load('models','Labtest_manage');
+            $result= $model->displayById($id);
+            $_POST['test']=$result;
             $this->load('views','header');
             $this->load('views','update_test');
+
+            if(isset($_POST['Update'])){
+                $testName = $_POST['test_name'];
+                $description = $_POST['test_description'];
+                $price = $_POST['test_price'];
+
+                $model->update($id,$testName,$price,$description);
+            }
         }
 
         public function delete(){
+            $id = $_GET['id'];
+            $model= $this->load('models','Labtest_manage');
+            $result= $model->displayById($id);
+            $_POST['test']=$result;
             $this->load('views','header');
             $this->load('views','delete_test');
+
+            if(isset($_POST['Delete'])){
+                $testId = $_POST['id'];
+                $model->delete($testId);
+            }
         }
 
     }
