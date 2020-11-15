76c1f1906ccded5349b4892ca66987bed0c029d7
DanuAnjana <56577570+DanuAnjana@users.noreply.github.com>
2020-11-15 14:12:41 +0530

    update lab test views

--- views/view_test.php
+++ views/view_test.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html>
+<title>View</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href=<?php echo Router::base_url()."/files/style.css" ?>>
+        <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+        <script src=<?php echo Router::base_url().'/files/js/labtest.js'?> type="text/javascript"></script>
+    </head>
 
     <div style="background-image: linear-gradient(to left,  oldlace, rgba(255, 255, 255, 0)), url(<?php echo Router::base_url().'/files/icons/schedule_picture.png'?>);background-repeat:no-repeat">
         <div class ="container">
@@ -5,13 +14,13 @@
                 <div class="topic">View Lab Tests</div>
                 <div class = "search-bar">
                     <div class="site-search"> 
-                        <input type="text" placeholder="Test Id" name="id"> 
+                        <input type="text" id="id" placeholder="Test Id" name="id"> 
                     </div>      <!--site-search-->  <!--text-->
                     <div class="site-search"> 
-                        <input type="text" placeholder="Test Name" name="name"> 
+                        <input type="text" id="name" placeholder="Test Name" name="name"> 
                     </div>      <!--site-search-->  <!--date-->
-                        <div class="site-search"> 
-                    <button type = "submit" style="font-size:18px">Search</button> 
+                    <div class="site-search"> 
+                        <button id="search-btn" type ="submit" name="search" style="font-size:18px">Search</button> 
                     </div>      <!--site-search-->  <!--btn-->
                 </div>      <!--search-bar-->  
                 <table class="view-table">
