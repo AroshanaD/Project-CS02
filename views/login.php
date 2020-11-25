14cb8c970b185b713d22bc051611db3170e4c899
DanuAnjana <56577570+DanuAnjana@users.noreply.github.com>
2020-11-25 12:56:07 +0530

    did few changes

--- views/login.php
+++ views/login.php
@@ -9,10 +9,10 @@
         <link href="https://fonts.googleapis.com/css2?family=Chilanka&display=swap" rel="stylesheet">
     </head>
     <body style="background-image: url(<?php echo Router::base_url().'/files/icons/undraw_authentication_fsn5.png'?>);background-repeat:no-repeat">
-    <div class="main-nav" style="background: #9d9bfd">
+    <div class="main-nav" style="background: #a48df5">
             <div></div>
             <div></div>
-           <div>MedCaid Hospitals</div>
+           <div style="color:white">MedCaid Hospitals</div>
            <div>
                 <a href=<?php echo Router::site_url().'/main' ?>><button class="main-button">Home</button></a>
                 <a href=<?php echo Router::site_url()."/register" ?>><button class="main-button">Register</button></a>
@@ -36,8 +36,8 @@
                             <div class="input">
                                 <input type="password" name="password" required>
                             </div>
-                            <p><a href="#">Forget password?</a></p>
-                            <p><a href=<?php echo Router::site_url().'/register'?>>Not already an user</a></p>
+                            <div><a href="#">Forget password?</a></div>
+                            <div><a href=<?php echo Router::site_url().'/register'?>>Not already an user</a></div>
                         
                             <div class="btn-area">
                                 <button type="submit" name="login-submit" class = "submit-btn" >Login</button>
