f0ef4e9444a2d34e646faa9661d41754c1e0be33
DanuAnjana <56577570+DanuAnjana@users.noreply.github.com>
2020-12-13 13:29:39 +0530

    create nav bar and add to header

--- views/navbar.php
+++ views/navbar.php
@@ -1,4 +1,14 @@
-<div class="dropdown" style="float:right;margin-right:69px; margin-top:15px">
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href=<?php echo Router::base_url().'/files/style1.css'?>>
+    </head>
+
+    <body>
+    <div class="nav"> 
+                <div class="dropdown" style="float:right;margin-right:69px; margin-top:15px">
                     <img src=<?php echo Router::base_url().'/files/icons/profile.png'?> style="width:40px; height: 40px; ">
                     <div class="dropdown-content" style="position:fixed; min-width:fixed;">
                         <a href='<?php echo Router::site_url()."/user/change_details"?>' style="margin-top:0px; margin-bottom:0px ;min-width:fixed;"><img src=<?php echo Router::base_url().'/files/icons/user.png'?> style="width:20px; height: 20px; "> Profile</a></br>
@@ -9,3 +19,6 @@
                 </button>
                 <button id="close-icon">
                 </button>
+            </div>
+
+
