237b87a73438dd6eb737e5d367480878ef2d280f
GayashaDS <56672701+GayashaDS@users.noreply.github.com>
2020-12-13 20:14:53 +0530

    buttons  added

--- views/patient_test.php
+++ views/patient_test.php
@@ -41,6 +41,11 @@
                     <table>
                     
                     </table>
+
+                    <div>
+                        <input type="submit" value="Next" class="next-btn">
+                        <input type="submit" value="Previous" class="next-btn">
+                    </div>
                 </div>
 
             </form>
--- views/view_test.php
+++ views/view_test.php
@@ -48,6 +48,11 @@
             <table>
 
             </table>
+
+            <div>
+                <input type="submit" value="Next" class="next-btn">
+                <input type="submit" value="Previous" class="next-btn">
+            </div>
         </div>
 
         <div class="footer">All rights are reserved</div>
