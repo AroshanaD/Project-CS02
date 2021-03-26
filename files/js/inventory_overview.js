8ca22a4c7a0196668cca0c4a4c2b81062ad8db32
DanuAnjana <56577570+DanuAnjana@users.noreply.github.com>
2021-03-26 10:42:07 +0530

    add inventory report

--- files/js/inventory_overview.js
+++ files/js/inventory_overview.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-
     $.ajax({
         url: '../../index.php/Reports/inventory_overview',
         data: {},
@@ -31,6 +30,13 @@ function render_details(data){
             $(`<td id=${"report-td-2"}>`).append(data['sirup_count'])
             /*SELECT COUNT(drug_name) FROM stock where unitary_unit='sirup'*/
         ),
+
+        $(`<tr id=${"report-tr"} >`).append(
+            $(`<td id=${"report-td-1"}>`).append("Total number of vendors with orders"),
+            $(`<td id=${"report-td-2"}>`).append(data['count_vendor'])
+            /*SELECT COUNT(drug_name) FROM stock where unitary_unit='sirup'*/
+        ),
+
         $(`<tr id=${"report-tr"} >`).append(
             $(`<td id=${"report-td-1"}>`).append("Total expense for drugs"),
             $(`<td id=${"report-td-2"}>`).append(data['expense'])
@@ -44,7 +50,7 @@ function render_details(data){
         ),
         $(`<tr id=${"report-tr"} >`).append(
             $(`<td id=${"report-td-1"}>`).append("Expected profit from drugs"),
-            $(`<td id=${"report-td-2"}>`).append(data['income']-data['expense'])
+            $(`<td id=${"report-td-2"}>`).append((data['income']-data['expense']).toFixed(2))
             /*SELECT SUM(unitary_value*selling_price*quantity) AS 'expense' FROM stock*/
         ),
         $(`<tr id=${"report-tr"} >`).append(
@@ -63,5 +69,28 @@ function render_details(data){
             /*SELECT COUNT(drug_name) FROM `stock` WHERE expire_date < NOW(); */
         ),
 
+        
+
     )
+    $('#day').append(
+        $(`<tr id=${"report-tr"} >`).append(
+            $(`<td id=${"report-td-3"} style="font-weight:bold;border: 1px black solid">`).append("br_id"),
+            $(`<td id=${"report-td-4"} style="font-weight:bold; border: 1px black solid">`).append("drug name"),
+            $(`<td id=${"report-td-5"} style="font-weight:bold;border: 1px black solid">`).append("Selling price"),
+            $(`<td id=${"report-td-6"} style="font-weight:bold;border: 1px black solid">`).append("quantity"),
+            $(`<td id=${"report-td-7"} style="font-weight:bold;border: 1px black solid">`).append("expected profit"),    
+        ),
+    )
+for(i=0;i<data['drug_details'].length;i++){
+    $('#day').append(
+        $(`<tr id=${"report-tr"} >`).append(
+            $(`<td id=${"report-td-3"} style="border: 1px black solid">`).append(data['drug_details'][i]['br_id']),
+            $(`<td id=${"report-td-4"} style="border: 1px black solid">`).append(data['drug_details'][i]['drug_name']),
+            $(`<td id=${"report-td-5"} style="border: 1px black solid">`).append(data['drug_details'][i]['selling_price']),
+            $(`<td id=${"report-td-6"} style="border: 1px black solid">`).append(data['drug_details'][i]['quantity']),
+            $(`<td id=${"report-td-7"} style="border: 1px black solid">`).append((data['drug_details'][i]['drugIncome']-data['drug_details'][i]['drugExpense']).toFixed(2)),    
+        ),
+    )
+}
+    
 }
