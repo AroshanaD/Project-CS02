de3467a6b3a1dec4b03c89f6d2443416f22fc0e2
GayashaDS <56672701+GayashaDS@users.noreply.github.com>
2021-03-28 08:23:01 +0530

    modified few

--- files/js/labtest_overview.js
+++ files/js/labtest_overview.js
@@ -23,6 +23,18 @@ function render_details(data) {
         $(`<tr id=${"report-tr"} >`).append(
             $(`<td id=${"report-td-1"}>`).append("Total number of Lab Tests"),
             $(`<td id=${"report-td-2"}>`).text(data['test_count'])
+        ),
+        $(`<tr id=${"report-tr"} >`).append(
+            $(`<td id=${"report-td-1"}>`).append("Total no.of tests for male patients"),
+            $(`<td id=${"report-td-2"}>`).append(data['male_test'])
+        ),
+        $(`<tr id=${"report-tr"} >`).append(
+            $(`<td id=${"report-td-1"}>`).append("Total no.of tests for female patients"),
+            $(`<td id=${"report-td-2"}>`).append(data['female_test'])
+        ),
+        $(`<tr id=${"report-tr"} >`).append(
+            $(`<td id=${"report-td-1"}>`).append("Total income from Lab Tests"),
+            $(`<td id=${"report-td-2"}>`).append(data['tot_income'])
         )
     )
 }
--- models/Report_Data.php
+++ models/Report_Data.php
@@ -212,6 +212,28 @@
                 $stmt->execute();
                 $patient_count=$stmt->fetch();
                 $result['test_count']=$patient_count['test_count'];
+
+                $query = "SELECT COUNT(patient_name) AS 'male' FROM lab_test
+                    WHERE patient_gender = 'male'";
+                $stmt = $pdo->prepare($query);
+                $stmt->execute();
+                $data = $stmt->fetch();
+                $result['male_test'] = $data['male_test'];
+
+                $query = "SELECT COUNT(patient_name) AS 'female' FROM lab_test
+                    WHERE patient_gender = 'female'";
+                $stmt = $pdo->prepare($query);
+                $stmt->execute();
+                $data = $stmt->fetch();
+                $result['female_test'] = $data['female_test'];
+
+                $query = "SELECT SUM(cost) AS 'tot_income';
+                $stmt = $pdo->prepare($query);
+                $stmt->execute();
+                $data = $stmt->fetch();
+                $result['tot_income'] = $data['tot_income'];
+
+                
         }
     }
 
