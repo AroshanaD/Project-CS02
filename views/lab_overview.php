9b584b0d3e77b6743270f8826276cfcba13a72ed
GayashaDS <56672701+GayashaDS@users.noreply.github.com>
2021-03-30 17:30:33 +0530

    add few changes

--- views/lab_overview.php
+++ views/lab_overview.php
@@ -18,13 +18,16 @@
             include $path; ?>
 
             <div class="report">
+            <div class="topic" style="width:100%; text-align:center">Laboratory Report</div>
+            <div class="report" id="inner">
                 <div class="report-paper">
+                <div class="content">
                     <div class="heading">
                         Lab Test Overview Report </br></br>
                         <p style="font-size: 16px; font-weight: normal; text-align: left">
                             <?php echo $_POST['from_date']; ?> --- <?php echo $_POST['to_date']; ?> </p>
                     </div>
-                    <div class="content">
+                    
                         <table class="report-tb" id="app">
 
                         </table>
